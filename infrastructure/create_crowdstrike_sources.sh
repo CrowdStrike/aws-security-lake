f32ee86c05b1da240cf5b8fd1e21e2a9ad915ee7
ffalor <35144141+ffalor@users.noreply.github.com>
2023-06-05 16:10:00 -0500

    docs: Remove creation of IAM Role from bash script
    This pull request entails the decoupling of the IAM Role creation process, which FDR assumes to write logs into managed S3 buckets, from the bash script responsible for creating the custom sources. The readme file has also been updated to reflect these changes.

--- infrastructure/create_crowdstrike_sources.sh
+++ infrastructure/create_crowdstrike_sources.sh
@@ -14,10 +14,7 @@ SUPPORTED_OCSF_CLASSES=(
 SECURITY_LAKE_REGION=
 GLI_ARN=
 ACCOUNT_ID=
-CFT_ROLE_NAME="CrowdStrike-AmazonSecurityLake-CustomSourceRole"
-CFT_BUCKET_NAME=
-CFT_EXTERNALID="CrowdStrikeCustomSource"
-CFT_STACKNAME="CrowdStrike-AmazonSecurityLake-CustomSourceRole"
+ROLE_EXTERNALID="CrowdStrikeCustomSource"
 
 # jq is required
 if ! command -v jq &> /dev/null
@@ -26,25 +23,10 @@ then
     exit 1
 fi
 
-# Check if we have a "ROLE_NAME" set in an environment variable
-if [[ -z "${ROLE_NAME}" ]]; then
-    echo "Using default role name: ${CFT_ROLE_NAME} (set ROLE_NAME env var to override)"
-else
-    CFT_ROLE_NAME="${ROLE_NAME}"
-fi
-
-# Check if we have a "EXTERNALID" set in an environment variable
 if [[ -z "${EXTERNALID}" ]]; then
-    echo "Using default external ID: ${CFT_EXTERNALID} (set EXTERNALID env var to override)"
+    echo "Using default external ID: ${ROLE_EXTERNALID} (set EXTERNALID env var to override)"
 else
-    CFT_EXTERNALID="${EXTERNALID}"
-fi
-
-# Check if we have a "CFT_STACKNAME" set in an environment variable
-if [[ -z "${STACKNAME}" ]]; then
-    echo "Using default CloudFormation stack name: ${CFT_STACKNAME} (set STACKNAME env var to override)"
-else
-    CFT_STACKNAME="${CFT_STACKNAME}"
+    ROLE_EXTERNALID="${EXTERNALID}"
 fi
 
 # Check if we have a "AWS region" set in an environment variable before prompting...
@@ -59,14 +41,6 @@ if [[ -z "${AWS_ACCOUNT_ID}" ]]; then
     read -p "[?] AWS Account ID: " ACCOUNT_ID
 else
     ACCOUNT_ID="${AWS_ACCOUNT_ID}"
-fi
-
-
-# Check if we have a "BUCKET_NAME" set in an environment variable before prompting...
-if [[ -z "${BUCKET_NAME}" ]]; then
-    read -p "[?] S3 Bucket name to write source to: " BUCKET_NAME
-else
-    BUCKET_NAME="${BUCKET_NAME}"
 fi
 
 read -p "[?] ARN of IAM Role that has permissions to Invoke Glue: " GLI_ARN
@@ -100,18 +74,8 @@ for klass in ${SUPPORTED_OCSF_CLASSES[@]}; do
 
     echo "[+] Creating ${source_name}..."
     aws securitylake create-custom-log-source \
-        --configuration "{\"crawlerConfiguration\":{\"roleArn\":\"$GLI_ARN\"},\"providerIdentity\":{\"externalId\":\"$CFT_EXTERNALID\",\"principal\":\"$ACCOUNT_ID\"}}" \
+        --configuration "{\"crawlerConfiguration\":{\"roleArn\":\"$GLI_ARN\"},\"providerIdentity\":{\"externalId\":\"$ROLE_EXTERNALID\",\"principal\":\"$ACCOUNT_ID\"}}" \
         --event-classes "[\"$klass\"]" \
         --source-name ${source_name} \
         --region ${SECURITY_LAKE_REGION}
 done
-
-aws cloudformation create-stack \
-    --stack-name ${CFT_ROLE_NAME} \
-    --template-body file://infrastructure/iam_role.yaml \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --parameters ParameterKey=ExternalId,ParameterValue=${CFT_EXTERNALID} \
-                 ParameterKey=BucketName,ParameterValue=${CFT_BUCKET_NAME} \
-                 ParameterKey=RoleName,ParameterValue=${CFT_ROLE_NAME} \
-                 ParameterKey=AccountId,ParameterValue=${ACCOUNT_ID} \
-    --region ${SECURITY_LAKE_REGION}
